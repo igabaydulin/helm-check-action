78e1e13d0852d41ab685a78fb386d0424a8d2108
Igor Gabaydulin <igabaydulin@gmail.com>
2019-03-11 00:24:43 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM alpine/helm
+
+LABEL version="0.1.0"
+LABEL repository="https://github.com/igabaydulin/helm-check-action"
+LABEL maintainer="Igor Gabaydulin"
+
+LABEL com.github.actions.name="helm check action"
+LABEL com.github.actions.description="Checks if helm's templates are valid"
+LABEL com.github.actions.icon="cloud"
+LABEL com.github.actions.color="blue"
+
+ADD entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,89 @@
+#!/bin/bash -l
+
+function printDelimeter {
+  echo "----------------------------------------------------------------------"
+}
+
+function printLargeDelimeter {
+  echo -e "\n\n------------------------------------------------------------------------------------------\n\n"
+}
+
+function printStepExecutionDelimeter {
+  echo "----------------------------------------"
+}
+
+function displayInfo {
+  printDelimeter
+  echo
+  HELM_CHECK_VERSION="v0.1.0"
+  HELM_CHECK_SOURCES="https://github.com/igabaydulin/"
+  echo "Helm-Check $HELM_CHECK_VERSION"
+  echo -e "Source code: $HELM_CHECK_SOURCES"
+  echo
+  printDelimeter
+}
+
+function helmLint {
+  echo -e "\n\n\n"
+  echo -e "1. Checking a chart for possible issues\n"
+  if [ -z "$CHART_LOCATION" ]; then
+    echo "Skipped due to condition: \$CHART_LOCATION is not provided"
+    return -1
+  fi
+  echo "helm lint $CHART_LOCATION"
+  printStepExecutionDelimeter
+  helm lint "$CHART_LOCATION"
+  HELM_LINT_EXIT_CODE=$?
+  printStepExecutionDelimeter
+  if [ $HELM_LINT_EXIT_CODE -eq 0 ]; then
+    echo "Result: SUCCESS"
+  else
+    echo "Result: FAILED"
+  fi
+  return $HELM_LINT_EXIT_CODE
+}
+
+function helmTemplate {
+  printLargeDelimeter
+  echo -e "2. Trying to render templates with provided values\n"
+  if [[ "$1" -eq 0 ]]; then
+    if [ -n "$CHART_VALUES" ]; then
+      echo "helm template --values $CHART_VALUES $CHART_LOCATION"
+      printStepExecutionDelimeter
+      helm template --values "$CHART_VALUES" "$CHART_LOCATION"
+      HELM_TEMPLATE_EXIT_CODE=$?
+      printStepExecutionDelimeter
+      if [ $HELM_TEMPLATE_EXIT_CODE -eq 0 ]; then
+        echo "Result: SUCCESS"
+      else
+        echo "Result: FAILED"
+      fi
+      return $HELM_TEMPLATE_EXIT_CODE
+    else
+      printStepExecutionDelimeter
+      echo "Skipped due to condition: \$CHART_VALUES is not provided"
+      printStepExecutionDelimeter
+    fi
+  else
+    echo "Skipped due to failure: Previous step has failed"
+    return $1
+  fi
+  return 0
+}
+
+function totalInfo {
+  printLargeDelimeter
+  echo -e "3. Summary\n"
+  if [[ "$1" -eq 0 ]]; then
+    echo "Examination is completed; no errors found!"
+    exit 0
+  else
+    echo "Examination is completed; errors found, check the log for details!"
+    exit 1
+  fi
+}
+
+displayInfo
+helmLint
+helmTemplate $?
+totalInfo $?
